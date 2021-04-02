1926014e5f016cdbe2f43238bc9914a807b38c10
bfeuling01 <bfeuling@gmail.com>
2021-04-02 14:12:01 -0500

    adding a new demo app folder

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a,b) {
+    return a - b;
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+function modulo(a,b) {
+    return a % b;
+}
+
+exports.modulo = modulo;
+exports.add = add;
+exports.subtract = subtract;
+exports.multiply = multiply;
+exports.divide = divide;
